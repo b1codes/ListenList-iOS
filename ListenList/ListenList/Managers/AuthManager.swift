78bc23592bc0bc2aadeecf010f570459a0763d0e
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-09-08 10:30:33 -0400

    little comment tweak

--- ListenList/ListenList/Managers/AuthManager.swift
+++ ListenList/ListenList/Managers/AuthManager.swift
@@ -5,14 +5,6 @@
 //  Created by Brandon Lamer-Connolly on 8/1/25.
 //
 
-
-//
-//  AuthManager.swift
-//  ListenList
-//
-//  Created by Brandon Lamer-Connolly on 10/12/24.
-//
-
 import Foundation
 import KeychainSwift
 
