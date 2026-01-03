1359260875de17e5fa3269084a9092c0e25060b5
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2026-01-03 15:51:04 -0500

    updated icon

--- ListenList/ListenList/AuthorizationView.swift
+++ ListenList/ListenList/AuthorizationView.swift
@@ -97,7 +97,7 @@ struct AuthorizationView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Image("Image")
+                Image("AppIcon")
                     .resizable()
                     .cornerRadius(30.0)
                     .scaledToFill()
